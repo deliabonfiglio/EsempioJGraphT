c3f605dcc04cf9a0cfb4c99af6b1108cd78b7333
Delia <Pietro@Pcstudio>
2017-04-25 21:52:38 +0200

    commenti personali aggiunti

--- EsempiGrafi/it.polito.tdp.grafi/EsempioGrafo.java
+++ EsempiGrafi/it.polito.tdp.grafi/EsempioGrafo.java
@@ -6,110 +6,85 @@ import org.jgrapht.graph.DirectedWeightedMultigraph;
 import org.jgrapht.graph.SimpleGraph;
 
 public class EsempioGrafo {
-
-	/*
-	 * grafo non diretto, semplice e non pesato
-	 */
-	void esempio1() {
-		System.out.println("Esempio 1");
-		
+/*
+ * grafo non diretto, semplice e non pesato
+ * oggetto di tipo simple graph
+ */
+	void esempio1(){
 		UndirectedGraph<Integer, DefaultEdge> grafo = new SimpleGraph<Integer, DefaultEdge>(DefaultEdge.class);
-
-		// Aggiungo i vertici
-		for (int i = 1; i <= 6; i++)
+		//voglio creare un grafo
+		//aggiungo i vertici
+		for(int i=1; i<=6; i++)
 			grafo.addVertex(i);
-
-		// stampo il grafo
-		System.out.println(grafo);
-
-		// Aggiunto gli archi
-		grafo.addEdge(1, 2);
-		grafo.addEdge(2, 3);
-		grafo.addEdge(1, 3);
-
-		grafo.addEdge(5, 6);
+		System.out.println(grafo.toString());
+		//aggiungo archi
+		grafo.addEdge(1,2);
+		grafo.addEdge(2,3);
+		grafo.addEdge(1,3);
 		
-		// stampo il grafo
-		System.out.println(grafo);
-
-		// Questo arco verrÃ  scartato
-		grafo.addEdge(5, 6);
-
-		// stampo il grafo
-		System.out.println(grafo);
+		grafo.addEdge(5,6);
+		System.out.println(grafo.toString());
+		
+		//questo arco viene scartato xk già esiste
+		grafo.addEdge(5,6);
 	}
-
 	/*
-	 * grafo diretto, multi, pesato
+	 * grafo diretto, multi, e pesato
 	 */
-	void esempio2() {
-		System.out.println("Esempio 2");
-
-		DirectedWeightedMultigraph<Integer, DefaultWeightedEdge> grafo = new DirectedWeightedMultigraph<Integer, DefaultWeightedEdge>(DefaultWeightedEdge.class);
-
-		// Aggiungo i vertici
-		for (int i = 1; i <= 6; i++)
+	void esempio2(){
+		DirectedWeightedMultigraph<Integer, DefaultWeightedEdge>grafo= new 	DirectedWeightedMultigraph<Integer, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+		//voglio creare un grafo
+		//aggiungo i vertici
+		for(int i=1; i<=6; i++)
 			grafo.addVertex(i);
-
-		System.out.println(grafo);
-
-		// Aggiungo gli archi
+		
+		System.out.println(grafo.toString());
+		//aggiungo archi
+		grafo.addEdge(1,2);
+		grafo.addEdge(2,3);
+		grafo.addEdge(1,3);
+		
+		grafo.addEdge(5,6);
+		System.out.println(grafo.toString());
+		grafo.addEdge(5,6);
+		
+		//oppure cosi iterando sugli archi
 		DefaultWeightedEdge e = grafo.addEdge(1, 2);
-		grafo.setEdgeWeight(e, 0.5);
-
-		e = grafo.addEdge(2, 3);
 		grafo.setEdgeWeight(e, 0.2);
-
-		e = grafo.addEdge(1, 3);
-		grafo.setEdgeWeight(e, 0.1);
-
-		e = grafo.addEdge(5, 6);
-		grafo.setEdgeWeight(e, 0.6);
-
-		System.out.println(grafo);
-
-		grafo.addEdge(5, 6);
-
-		System.out.println(grafo);
-
-		System.out.println("\nArchi del grafo:");
 		
-		// Itero sugli archi
-		for (DefaultWeightedEdge arch : grafo.edgeSet()) {
+		for(DefaultWeightedEdge arch: grafo.edgeSet()){
 			System.out.println(arch);
-			System.out.format("source: %d - target: %d peso: %f\n", grafo.getEdgeSource(arch), grafo.getEdgeTarget(arch), grafo.getEdgeWeight(arch));
+			//System.out.println(grafo.getEdgeWeight(arch));
+			System.out.format("%d-%d peso: %f\n", grafo.getEdgeSource(arch),
+											grafo.getEdgeTarget(arch),
+											grafo.getEdgeWeight(arch));
 		}
-
-		System.out.println("\nNodi del grafo:");
-		
-		// Itero sui vertici
-		for (Integer vertex : grafo.vertexSet()) {
+		//itero sui vertici
+		for(Integer vertex: grafo.vertexSet()){
+			//archi incidenti sul v
 			System.out.format("vertex: %d\n", vertex);
-			
-			for (DefaultWeightedEdge arch : grafo.edgesOf(vertex)) {
-
-				System.out.format("-- adiacent vertex: %d\n", Graphs.getOppositeVertex(grafo, arch, vertex));
-
-				if (grafo.getEdgeSource(arch) == vertex) {
+			for(DefaultWeightedEdge arch: grafo.edgesOf(vertex)){
+				//posso usare questo
+				System.out.format("---adiacent vertex: %d\n", Graphs.getOppositeVertex(grafo, arch, vertex));
+				//oppure questo ma ottengo lo stesso risultato
+				if(grafo.getEdgeSource(arch)== vertex){
 					System.out.format("adiacent vertex: %d\n", grafo.getEdgeTarget(arch));
 				} else {
-					System.out.format("adiacent vertex: %d\n", grafo.getEdgeSource(arch));
+					System.out.format("adiacent vertex: %d\n",grafo.getEdgeSource(arch));
 				}
-
 			}
-			System.out.format("inDegree: %d -- outDegree %d\n\n", grafo.inDegreeOf(vertex), grafo.outDegreeOf(vertex));
-		}
-
-		System.out.println("\nneighborList of 3");
-		System.out.println(Graphs.neighborListOf(grafo, 3));
-
-		System.out.println("\nsuccessor of 3");
-		System.out.println(Graphs.successorListOf(grafo, 3));
-
-		System.out.println("\npredecessor of 3");
-		System.out.println(Graphs.predecessorListOf(grafo, 3));
+			//stampare per ogni ertice il grado
+			System.out.format("inDegree: %d -- outDegree: %d\n",
+								grafo.inDegreeOf(vertex),
+								grafo.outDegreeOf(vertex));			
+		}	
+		
+		System.out.println("\nNeighbours of 3:");
+		System.out.println(Graphs.neighborListOf(grafo,  3));
+		
+		System.out.println("\nSuccessors of 3:");
+		System.out.println(Graphs.successorListOf(grafo,  3));
 	}
-
 	public static void main(String[] args) {
 
 		EsempioGrafo eg = new EsempioGrafo();
